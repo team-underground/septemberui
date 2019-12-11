a0ea8b03df5a5a9dad956227ab8bd29ad8723a73
abhishek sarmah <abhisheksarmah660@gmail.com>
2019-12-11 12:55:42 +0530

    initial commit

---
+++ src/lib-components/index.js
@@ -0,0 +1,17 @@
+/* eslint-disable import/prefer-default-export */
+
+// Export tui base components
+export { default as Alert } from "./Alert.vue";
+export { default as Avatar } from "./Avatar.vue";
+export { default as Badge } from "./Badge.vue";
+export { default as BasicTable } from "./BasicTable.vue";
+export { default as Card } from "./Card.vue";
+export { default as Dropdown } from "./Dropdown.vue";
+export { default as FileInput } from "./FileInput.vue";
+export { default as Heading } from "./Heading.vue";
+export { default as LoadingButton } from "./LoadingButton.vue";
+export { default as RadioInput } from "./RadioInput.vue";
+export { default as SearchInput } from "./SearchInput.vue";
+export { default as SelectInput } from "./SelectInput.vue";
+export { default as SwitchInput } from "./SwitchInput.vue";
+export { default as TextInput } from "./TextInput.vue";
---
+++ src/lib-dev.vue
@@ -0,0 +1,23 @@
+<script>
+import { Alert } from "@/entry";
+export default {
+	name: "SeptemberuiLibDev", // vue library dev component
+	components: {
+		Alert
+	}
+};
+</script>
+
+<template>
+	<div class="septemberui-lib-dev">
+		<alert>asdjjjk</alert>
+	</div>
+</template>
+
+<style scoped>
+.septemberui-lib-dev {
+	display: block;
+	width: 400px;
+	margin: 25px auto;
+}
+</style>
